6f8ab8f2dbb77647591488a94943655b0ea5e0ed
Alex R <y0105705@tu-bs.de>
2022-05-30 12:22:12 +0200

    Change arg management

--- src/miner.lua
+++ src/miner.lua
@@ -1,10 +1,10 @@
 -- Makes a mineshaft with some optional features --
 -- Made by @Sharpened99 --
--- alpha0.1 --
+-- alpha0.1.1 --
 
 -- Variables --
 local hallLength
-local branchDepth
+local branchDepth = 0
 local torchPlacement = false
 
 -- Arguments --
@@ -12,30 +12,38 @@ local torchPlacement = false
 
 local tArgs = {...}
 
-if #tArgs < 2 or #tArgs > 3 then
+if #tArgs < 1 or #tArgs > 3 then
     print("Usage: <hallLength> <branchDepth> <torchPlacement>")
+elseif  #tArgs == 1 then
+    assert(isInteger(tArgs[1]))
+
+    hallLength = tArgs[1]
 elseif #tArgs == 2 then
     assert(isInteger(tArgs[1]))
     assert(isInteger(tArgs[2]))
 
     hallLength = tArgs[1]
     branchDepth = tArgs[2]
-    if #tArgs == 3 then
-        assert(isBoolean(tArgs[3]))
-        torchPlacement = tArgs[3]
-    end
+elseif #tArgs == 3 then
+    assert(isInteger(tArgs[1]))
+    assert(isInteger(tArgs[2]))
+    assert(isBoolean(tArgs[3]))
 
-    -- Usage Errors --
-    if hallLength < 1 then
-        print("hallLength must be a positive number")
-    end
-    if branchDepth < 0 then
-        print("branchDepth must be either 0 (disables branches) or positive")
-    end
-    if torchPlacement ~= false and torchPlacement ~= true then
-        print("torchPlacement must be either omitted, true or false")
-        print("0 and 1 as stand-ins for false and true are acceptable")
-    end
+    hallLength = tArgs[1]
+    branchDepth = tArgs[2]
+    torchPlacement = tArgs[3]
+end
+
+ -- Usage Errors --
+if hallLength < 1 then
+    print("hallLength must be a positive number")
+end
+if branchDepth < 0 then
+    print("branchDepth must be either 0 (disables branches) or positive")
+end
+if torchPlacement ~= false and torchPlacement ~= true then
+    print("torchPlacement must be either omitted, true or false")
+    print("0 and 1 as stand-ins for false and true are acceptable")
 end
 
 function isInteger(n)
